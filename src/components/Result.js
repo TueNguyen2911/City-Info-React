a8c6b81876dfefb2844bbbbb99097a77e8177ed9
Tue <tnguyen187@myseneca.ca>
2021-08-02 19:12:58 -0400

    got animatin working

--- src/components/Result.js
+++ src/components/Result.js
@@ -47,10 +47,10 @@ const Result = ({searchData}) => {
                 <Card  key={data.id} id={data.id}>
                     <Card.Body >
                     <Card.Title>{data.name}</Card.Title>
-                    <div className="detailed-info-container">
-                        <div className={(data.isToggled ? "detailed-info-container-show" : 'detailed-info-container-close')}>
+                    <div className={"detailed-info-container"}>
+                        <div className={(data.isToggled ? "detailed-info-container-show" : "detailed-info-container-close")}>
                             Hi
-                            <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
+                            {/* <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" /> */}
 
                         </div>
                     </div>
